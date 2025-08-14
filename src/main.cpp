82c8dbb2f01cf4d031431f351c51e580cd9c94f6
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-14 13:12:06 +0100

    vector to support multipul boids

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
 #include "../include/main.h"
 #include "../include/utils.h"
 #include "raylib.h"
+#include <bits/stdc++.h>
+#include <vector>
+
+std::vector<boid> boids = {{10, 0}, {1, 25}, {19, 25}};
 
 int main(void) {
   InitWindow(800, 450, "raylib [core] exampwle - basic window");
-
   SetTargetFPS(10);
-
-  struct boid b = {{10, 0}, {1, 25}, {19, 25}};
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    create_boid(b.v1, b.v2, b.v3);
+    int amount_of_boids = boids.size();
 
-    b.v1.x += .5;
-    b.v1.y += .5;
+    for (int i = 0; i < amount_of_boids; i++) {
+      boids[i].v1.x += 5;
+      boids[i].v1.y += 5;
 
-    b.v2.x += .5;
-    b.v2.y += .5;
+      boids[i].v2.x += 5;
+      boids[i].v2.y += 5;
 
-    b.v3.x += .5;
-    b.v3.y += .5;
+      boids[i].v3.x += 5;
+      boids[i].v3.y += 5;
+    }
 
     EndDrawing();
   }
--- src/utils.cpp
+++ src/utils.cpp
@@ -3,4 +3,6 @@
 
 void create_boid(Vector2 v1, Vector2 v2, Vector2 v3) {
   DrawTriangle(v1, v2, v3, BLACK);
+
+  boids.push_back({v1, v2, v3});
 }
