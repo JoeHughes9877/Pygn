6cc57716b55ee3c3504b40f072fbb6882b8686c6
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-14 02:01:05 +0100

    omg it moves

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 int main(void) {
   InitWindow(800, 450, "raylib [core] exampwle - basic window");
 
+  SetTargetFPS(10);
+
   struct boid b = {{10, 0}, {1, 25}, {19, 25}};
 
   while (!WindowShouldClose()) {
@@ -12,6 +14,15 @@ int main(void) {
     ClearBackground(RAYWHITE);
 
     create_boid(b.v1, b.v2, b.v3);
+
+    b.v1.x += .5;
+    b.v1.y += .5;
+
+    b.v2.x += .5;
+    b.v2.y += .5;
+
+    b.v3.x += .5;
+    b.v3.y += .5;
 
     EndDrawing();
   }
