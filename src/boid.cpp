b1a453f071ddae4c4e12465f84421bd935c76490
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-15 22:35:00 +0100

    triganaomity

--- src/boid.cpp
+++ src/boid.cpp
@@ -1,17 +1,23 @@
 #include "../include/boid.h"
 #include "raylib.h"
 #include <cmath>
+#include <cstdlib>
 #include <vector>
 
 float size = 10;
 
+void init_movement(int index);
+
 void create_boid(Vector2 pos) {
   boid b;
   b.position = pos;
-  b.velocity = {0, 1};
-  b.angle = 0; // for now
+  b.velocity = {0, 0};
+  b.angle = 0;
 
   boids.push_back(b);
+  int arr_len = boids.size();
+
+  init_movement(arr_len - 1);
 }
 
 void render_boid(Vector2 pos) {
@@ -22,9 +28,16 @@ void render_boid(Vector2 pos) {
   DrawTriangle(v1, v2, v3, BLACK);
 }
 
-void move_boid(boid b, int index) {
-  boids[index].angle = atan2(b.velocity.y, b.velocity.x);
+void init_movement(int index) {
+  float angle = static_cast<float>(rand()) / RAND_MAX * 2 * 3.1415926f;
+  boids[index].angle = angle;
 
+  float speed = 2.0f;
+  boids[index].velocity.x = cos(angle) * speed;
+  boids[index].velocity.y = sin(angle) * speed;
+}
+
+void move_boid(int index) {
   boids[index].position.x += boids[index].velocity.x;
   boids[index].position.y += boids[index].velocity.y;
 }
