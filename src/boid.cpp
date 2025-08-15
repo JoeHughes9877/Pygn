18057a72f02e27f335427eefd3ba822b3519fbfb
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-15 23:59:56 +0100

    they vibrate aggressivly this was to funny not to commit

--- src/boid.cpp
+++ src/boid.cpp
@@ -58,7 +58,7 @@ void detect_other_boids(boid b) {
         CheckCollisionSpheres(boid_pos_3D, 5.0f, other_boid_pos_3D, 0.5f);
 
     if (detected) {
-      // TODO
+      init_movement(i);
     }
   }
 }
