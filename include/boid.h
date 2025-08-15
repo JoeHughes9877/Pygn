375f2d6b66504979410707e97189a4ca513c8529
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-16 00:34:49 +0100

    pointers

--- include/boid.h
+++ include/boid.h
@@ -13,8 +13,8 @@ extern std::vector<boid> boids;
 void create_boid(Vector2 pos);
 void move_boid(int index);
 void render_boid(Vector2 pos);
-void detect_other_boids(boid b);
+void detect_other_boids(boid &b);
 
-void alignment(std::vector<boid> &vec, boid b);
-void cohesion(std::vector<boid> &vec, boid b);
-void separation(std::vector<boid> &vec, boid b);
+void alignment(std::vector<boid> &vec, boid &b);
+void cohesion(std::vector<boid> &vec, boid &b);
+void separation(std::vector<boid> &vec, boid &b);
