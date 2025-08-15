f1994cae89143f9fb801bc9a8588487b25fd4827
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-16 00:18:31 +0100

    implicit

--- include/boid.h
+++ include/boid.h
@@ -14,3 +14,7 @@ void create_boid(Vector2 pos);
 void move_boid(int index);
 void render_boid(Vector2 pos);
 void detect_other_boids(boid b);
+
+void alignment(std::vector<boid> &vec, boid b);
+void cohesion(std::vector<boid> &vec, boid b);
+void separation(std::vector<boid> &vec, boid b);
