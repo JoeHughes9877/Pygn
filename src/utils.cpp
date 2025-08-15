7e25bc6a1f5b26ceece849e78c9a8987f7b4a41a
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-15 18:47:30 +0100

    create boid, render boid plans for move boid

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,32 @@
 #include "../include/main.h"
 #include "raylib.h"
+#include <vector>
 
-void render_boid(Vector2 v1, Vector2 v2, Vector2 v3) {
+Vector2 move_boid();
+
+float size = 10;
+
+void create_boid(Vector2 pos) {
+  Vector2 v1 = {pos.x, pos.y - size};
+  Vector2 v2 = {pos.x - size, pos.y + size};
+  Vector2 v3 = {pos.x + size, pos.y + size};
+
+  boid b;
+  b.position = pos;
+  b.velocity = {0, 0};
+  b.angle = 0; // for now
+
+  boids.push_back(b);
+}
+
+void render_boid(Vector2 pos) {
+  Vector2 v1 = {pos.x, pos.y - size};
+  Vector2 v2 = {pos.x - size, pos.y + size};
+  Vector2 v3 = {pos.x + size, pos.y + size};
+
   DrawTriangle(v1, v2, v3, BLACK);
+}
+
+Vector2 move_boid() {
+  // TODO
 }
