11e75229f0c8af5a24a1f459989e110d63796650
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-15 23:57:03 +0100

    implicit

--- include/boid.h
+++ include/boid.h
@@ -5,6 +5,7 @@ struct boid {
   Vector2 position;
   Vector2 velocity;
   float angle;
+  float detection_area;
 };
 
 extern std::vector<boid> boids;
@@ -12,3 +13,4 @@ extern std::vector<boid> boids;
 void create_boid(Vector2 pos);
 void move_boid(int index);
 void render_boid(Vector2 pos);
+void detect_other_boids(boid b);
