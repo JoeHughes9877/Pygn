e7c5006fd6b30b7066355a17cf855c4abe8b4689
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-16 18:11:18 +0100

    removed detect boids

--- include/boid.h
+++ include/boid.h
@@ -17,7 +17,6 @@ extern std::vector<boid> boids;
 void create_boid(Vector2 pos);
 void move_boid(int index);
 void render_boid(Vector2 pos);
-void detect_other_boids(boid &b);
 
 void alignment(std::vector<boid> &vec, boid &b);
 void cohesion(std::vector<boid> &vec, boid &b);
