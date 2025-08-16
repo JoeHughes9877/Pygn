568686efe59192a24c7b7b9d69c8893f9fcdc8eb
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-16 18:43:26 +0100

    fixed issue causingt boids to go to pos o, o

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,8 +7,8 @@ Vector2 average(std::vector<boid> &vec) {
   Vector2 total_value = {0, 0};
 
   for (int i = 0; i < arr_len; i++) {
-    total_value.x += vec[i].velocity.x;
-    total_value.y += vec[i].velocity.y;
+    total_value.x += vec[i].position.x;
+    total_value.y += vec[i].position.y;
   }
 
   return {total_value.x / arr_len, total_value.y / arr_len};
