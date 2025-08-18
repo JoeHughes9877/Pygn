44f1a271a1eca8c3a8e419b306664fc1a5e2688a
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-18 15:55:18 +0100

    nessessary changes untill direction is added

--- src/boid.cpp
+++ src/boid.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 float size = 10;
-const float MAX_SPEED = 10;
+const float MAX_SPEED = 5;
 const float MAX_STEERING = 2.5;
 
 void init_movement(int index);
@@ -16,19 +16,20 @@ void create_boid(Vector2 pos) {
   b.position = pos;
   b.velocity = {0, 0};
   b.angle = 0;
-  b.detection_area = 25.0f;
+  b.detection_area = 100.0f;
   b.steering.x = 0;
   b.steering.y = 0;
+
   boids.push_back(b);
   int arr_len = boids.size();
 
   init_movement(arr_len - 1);
 }
 
-void render_boid(Vector2 pos) {
-  Vector2 v1 = {pos.x, pos.y - size};
-  Vector2 v2 = {pos.x - size, pos.y + size};
-  Vector2 v3 = {pos.x + size, pos.y + size};
+void render_boid(boid b) {
+  Vector2 v1 = {b.position.x, b.position.y - size};
+  Vector2 v2 = {b.position.x - size, b.position.y + size};
+  Vector2 v3 = {b.position.x + size, b.position.y + size};
 
   DrawTriangle(v1, v2, v3, BLACK);
 }
@@ -50,13 +51,10 @@ void move_boid(int index) {
 
   boids[index].position.x += boids[index].velocity.x;
   boids[index].position.y += boids[index].velocity.y;
-
-  boids[index].steering = {0, 0};
 }
 
 Vector2 alignment(std::vector<boid> &vec, boid &b) {
   std::vector<boid> neighbours = find_neighbours(vec, b);
-
   Vector2 desired_velocity = average_velocity(neighbours);
   Vector2 steering = {0, 0};
 
@@ -93,30 +91,32 @@ Vector2 cohesion(std::vector<boid> &vec, boid &b) {
 }
 
 Vector2 separation(std::vector<boid> &vec, boid &b) {
-  int amount_of_boids = boids.size();
+  std::vector<boid> neighbours = find_neighbours(vec, b);
+
   Vector2 steering = {0, 0};
   int nearby_boids = 0;
 
-  for (int i = 0; i < amount_of_boids; i++) {
-    float dx = b.position.x - vec[i].position.x;
-    float dy = b.position.y - vec[i].position.y;
+  for (size_t i = 0; i < neighbours.size(); ++i) {
+    float dx = b.position.x - neighbours[i].position.x;
+    float dy = b.position.y - neighbours[i].position.y;
     float distance = std::sqrt(dx * dx + dy * dy);
 
-    if (&boids[i] != &b && distance < b.detection_area && distance > 0.0f) {
+    if (distance > 0.0f) {
       nearby_boids++;
-
       steering.x += dx / distance;
       steering.y += dy / distance;
     }
   }
-  // averageing result
+
+  // Averaging the result
   if (nearby_boids > 0) {
     steering.x /= nearby_boids;
     steering.y /= nearby_boids;
   }
 
-  float magnitude = sqrt(steering.x * steering.x + steering.y * steering.y);
-
+  // Normalize the steering force to prevent it from getting too large
+  float magnitude =
+      std::sqrt(steering.x * steering.x + steering.y * steering.y);
   if (magnitude > MAX_STEERING) {
     steering.x = steering.x * (MAX_STEERING / magnitude);
     steering.y = steering.y * (MAX_STEERING / magnitude);
