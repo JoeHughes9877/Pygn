286375e34125ba373d899d7b74328b2323c0ccb3
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-18 01:37:00 +0100

    created average vel and pos

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,7 @@
 #include "../include/boid.h"
 #include <vector>
 
-Vector2 average(std::vector<boid> &vec);
+Vector2 average_velocity(std::vector<boid> &vec);
+Vector2 average_position(std::vector<boid> &vec);
 void vector_normalisation(int index, const int MAX_SPEED);
+std::vector<boid> find_neighbours(std::vector<boid> &vec, boid b);
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,7 +2,19 @@
 #include <cmath>
 #include <vector>
 
-Vector2 average(std::vector<boid> &vec) {
+Vector2 average_velocity(std::vector<boid> &vec) {
+  int arr_len = vec.size();
+  Vector2 total_value = {0, 0};
+
+  for (int i = 0; i < arr_len; i++) {
+    total_value.x += vec[i].velocity.x;
+    total_value.y += vec[i].velocity.y;
+  }
+
+  return {total_value.x / arr_len, total_value.y / arr_len};
+}
+
+Vector2 average_position(std::vector<boid> &vec) {
   int arr_len = vec.size();
   Vector2 total_value = {0, 0};
 
@@ -23,4 +35,20 @@ void vector_normalisation(int index, const int MAX_SPEED) {
     boids[index].velocity.x *= scale;
     boids[index].velocity.y *= scale;
   }
+}
+
+std::vector<boid> find_neighbours(std::vector<boid> &vec, boid b) {
+  std::vector<boid> neighbours;
+  int amount_of_boids = boids.size();
+
+  for (int i = 0; i < amount_of_boids; i++) {
+    float dx = b.position.x - vec[i].position.x;
+    float dy = b.position.y - vec[i].position.y;
+    float distance = std::sqrt(dx * dx + dy * dy);
+
+    if (&vec[i] != &b && distance < b.detection_area && distance > 0.0f) {
+      neighbours.push_back(vec[i]);
+    }
+  }
+  return neighbours;
 }
