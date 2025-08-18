8c2e4105db8a2ec4168fb852bf1953a5dcf3add0
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-18 02:07:13 +0100

    #

--- include/boid.h
+++ include/boid.h
@@ -16,10 +16,11 @@ extern std::vector<boid> boids;
 
 void create_boid(Vector2 pos);
 void move_boid(int index);
-void render_boid(Vector2 pos);
+void render_boid(boid b);
 
 Vector2 alignment(std::vector<boid> &vec, boid &b);
 Vector2 cohesion(std::vector<boid> &vec, boid &b);
 Vector2 separation(std::vector<boid> &vec, boid &b);
+void wrap_boid(boid &b, float width, float height);
 
 #endif
