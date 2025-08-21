37674c4416d3cd48cce43d9d859fe6f78ef3dad7
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-21 21:42:15 +0100

    removed unnessessary or, changed for to be more c++ i guess???

--- src/main.cpp
+++ src/main.cpp
@@ -11,41 +11,38 @@ const int SCREEN_HEIGHT = 1080;
 
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Boids simulation");
-  SetTargetFPS(60);
+  SetTargetFPS(120);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLUE);
 
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) ||
-        IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
       Vector2 mouse_pos = GetMousePosition();
-
       create_boid(mouse_pos);
     }
 
-    int amount_of_boids = boids.size();
-    for (int i = 0; i < amount_of_boids; i++) {
+    for (auto &boid : boids) {
 
-      Vector2 alignment_force = alignment(boids, boids[i]);
-      Vector2 cohesion_force = cohesion(boids, boids[i]);
-      Vector2 separation_force = separation(boids, boids[i]);
+      Vector2 alignment_force = alignment(boids, boid);
+      Vector2 cohesion_force = cohesion(boids, boid);
+      Vector2 separation_force = separation(boids, boid);
 
       // Apply forces with weights
       float separation_weight = 3.5f;
       float alignment_weight = 1.0f;
       float cohesion_weight = 1.0f;
 
-      boids[i].steering.x += (separation_force.x * separation_weight) +
-                             (alignment_force.x * alignment_weight) +
-                             (cohesion_force.x * cohesion_weight);
-      boids[i].steering.y += (separation_force.y * separation_weight) +
-                             (alignment_force.y * alignment_weight) +
-                             (cohesion_force.y * cohesion_weight);
+      boid.steering.x += (separation_force.x * separation_weight) +
+                         (alignment_force.x * alignment_weight) +
+                         (cohesion_force.x * cohesion_weight);
+      boid.steering.y += (separation_force.y * separation_weight) +
+                         (alignment_force.y * alignment_weight) +
+                         (cohesion_force.y * cohesion_weight);
 
-      wrap_boid(boids[i], SCREEN_WIDTH, SCREEN_HEIGHT);
-      render_boid(boids[i]);
-      move_boid(i);
+      wrap_boid(boid, SCREEN_WIDTH, SCREEN_HEIGHT);
+      render_boid(boid);
+      move_boid(&boid);
     }
 
     EndDrawing();
