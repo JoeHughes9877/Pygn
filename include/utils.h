41198841651e90b5f0ba4e59f7feca8967fced85
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-21 23:01:55 +0100

    bool flags for averaging

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,7 @@
 #include "../include/boid.h"
 #include <vector>
 
-Vector2 average_velocity(std::vector<boid> &vec);
-Vector2 average_position(std::vector<boid> &vec);
+Vector2 average(std::vector<boid> &vec, bool use_velocity, bool use_position);
 void normalise_velocity(boid *b, const int MAX_SPEED);
-std::vector<boid> find_neighbours(std::vector<boid> &vec, boid b, float detect);
+std::vector<boid> find_neighbours(std::vector<boid> &vec, boid &b,
+                                  float detect);
